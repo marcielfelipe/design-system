c063af674ac31dadf2a7abfa736e9469c0e70945
Marciel Felipe <marciel.felipe@outlook.com>
2023-05-01 19:15:40 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Design system Ignite
+
+Design system desenvolvido no curso Ignite da Rocketseat
+
+## Components
+
+- [ ] Button
+- [ ] Heading
+- [ ] Text
+- [ ] TextInput
+- [ ] Textarea
+- [ ] Checkbox
+- [ ] Avatar
+- [ ] MultiStep
---
+++ packages/docs/.storybook/main.js
@@ -0,0 +1,23 @@
+/** @type { import('@storybook/react-webpack5').StorybookConfig } */
+const config = {
+  stories: [
+    "../src/stories/**/*.mdx", 
+    "../src/stories/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  core: {
+    builder: '@storybook/builder-vite',
+  },
+};
+export default config;
